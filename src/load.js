6f8f5770672060488c8fcc31270ecc6f8fd664fd
Christopher Hinstorff <chris@chinstorff.com>
2014-07-08 15:38:30 +0200

    add background (looks like snow. oh well)

--- src/load.js
+++ src/load.js
@@ -8,6 +8,7 @@ Game.Load.prototype = {
 	// load everything
 	game.load.spritesheet('blocks', 'assets/img/blocks.png', 21, 21, 12);
 	game.load.image('player', 'assets/img/player.png');
+	game.load.image('sky', 'assets/img/block-sky.png');
 
 	game.load.tilemap('level1', 'assets/js/levels/level1.json', null, Phaser.Tilemap.TILED_JSON);
     },
--- src/play.js
+++ src/play.js
@@ -6,21 +6,23 @@ A.playerGravity = 1000;
 
 Game.Play.prototype = {
     create: function () {
-	game.stage.backgroundColor = '#eee';
+	game.stage.backgroundColor = '#e5f2f3';
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 	this.createControls();
+
+	A.map = game.add.tilemap('level1');
+	A.map.addTilesetImage('blocks', 'blocks');
+	A.map.addTilesetImage('sky', 'sky');
+	A.layerSky = A.map.createLayer('sky');
+	A.layer = A.map.createLayer('blocks');
+	A.map.setCollisionBetween(8, 9, true, A.layer);
+	A.layer.resizeWorld();
 
 	A.player = game.add.sprite(50, 100, 'player');
 	A.player.anchor.setTo(0.5, -2.5);
 	game.physics.arcade.enable(A.player);
 	A.player.body.gravity.y = A.playerGravity;
 	A.player.body.collideWorldBounds = true;
-
-	A.map = game.add.tilemap('level1');
-	A.map.addTilesetImage('blocks', 'blocks');
-	A.map.setCollisionBetween(8, 9);
-	A.layer = A.map.createLayer('blocks');
-	A.layer.resizeWorld();
 
 	game.camera.follow(A.player);
 	game.camera.deadzone = new Phaser.Rectangle(A.w / 16 * 7, 0, A.w / 8, A.h);
